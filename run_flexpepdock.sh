3e987afb66e30947b56e17af537a57d73d9c98d2
Zaki Molvi <zakimolvi@users.noreply.github.com>
2023-02-16 18:59:08 -0500

    Create run_flexpepdock.sh

---
+++ run_flexpepdock.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Given an input structure of a peptide of interest threaded onto MHC, use Rosetta's FlexPepDock protocol to perform high-resolution docking.
+
+# rosetta path
+$ROSETTA3=~/rosetta_bin_mac_2020.08.61146_bundle/main/source
+
+#prepack input pdb and dock
+$ROSETTA3/bin/FlexPepDocking.macosclangrelease -flexpep_prepack -ex1 -ex2aro -use_input_sc -out:suffix _prepacked -out:no_nstruct_label -s input.pdb
+$ROSETTA3/bin/FlexPepDocking.macosclangrelease -pep_refine -nstruct 200 -ex1 -ex2aro -use_input_sc -out:suffix _docked -s input_prepacked.pdb
+
+#view score file
+pretty_tsv score.sc
